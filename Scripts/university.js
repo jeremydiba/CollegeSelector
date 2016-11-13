0329515d3196fe970d93d6dd3ebe841883573b2e
jeremydiba <jeremydibattista@gmail.com>
2016-11-12 19:31:53 -0500

    counter nonfunctional

--- Scripts/university.js
+++ Scripts/university.js
@@ -1,3 +1,16 @@
 /**
  * Created by jster on 11/12/2016.
  */
+
+var universityName, website, satMath, satReading, act, acceptanceRate, gpa;
+function  University(universityName, website, satMath, satReading, act, acceptanceRate, gpa) {
+    this.universityName = universityName;
+    this.website = website;
+    this.satMath = satMath;
+    this.satReading = satReading;
+    this.act = act;
+    this.acceptanceRate = acceptanceRate;
+    this.gpa = gpa;
+}
+
+
